96c0f19737ad68bbc41992e022369957b01135b7
Greg <g.rutherford10@gmail.com>
2018-11-12 13:15:07 +0000

    add characters from the API

--- src/Components/Characters.js
+++ src/Components/Characters.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 
 class Characters extends Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      results: []
+    }
+  }
+
+  componentDidMount(){
+    let url = "https://rickandmortyapi.com/api/character/"
+    fetch(url)
+    .then(response => response.json())
+    .then(results => this.setState({results: results["results"]}))
+    .catch(err => console.log(err));
+  }
+
   render() {
     return(
       <h1>Characters</h1>
